3a1df3bb16092b320104ce1ffca6000eb4557815
Anmol Panchal <panchalji9705@gmail.com>
2026-02-11 22:38:04 +0530

    using map function

--- Fragment/learningFragment/src/App.jsx
+++ Fragment/learningFragment/src/App.jsx
@@ -1,16 +1,13 @@
 // import './App.css'
 import React from "react";
 function App() {
-
+  let AITools = [ "ChatGPT", "DALL-E", "Codex", "Whisper", "CLIP", "Jukebox", "OpenAI Gym", "OpenAI Baselines", "OpenAI Spinning Up", "OpenAI Five", "OpenAI Robotics", "OpenAI Scholars", "OpenAI Fellows", "OpenAI Researcher in Residence", "OpenAI API", "OpenAI Codex API", "OpenAI DALL-E API", "OpenAI GPT-3 API", "OpenAI Whisper API" ];
   return (
    <React.Fragment>        
-      <h1>Fragment Learning</h1>
-      <ul class="list-group">
-        <li class="list-group-item">An item</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-        <li class="list-group-item">A fourth item</li>
-        <li class="list-group-item">And a fifth one</li>
+      <h1>AI Tools</h1>
+      {AITools.length ===0 ? <p>No AI Tools Found</p> : null}
+      <ul className="list-group">
+        {AITools.map((tool) => <li  key={tool} className="list-group-item">{tool}</li>)}
       </ul>
     </React.Fragment>
     );
